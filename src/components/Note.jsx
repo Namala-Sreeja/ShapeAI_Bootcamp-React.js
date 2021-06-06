2dafd1d491910486ae6daa9552fc1235101dc1ca
Namala-Sreeja <85444563+Namala-Sreeja@users.noreply.github.com>
2021-06-07 00:48:55 +0530

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note(){
+  return(
+    <div className="note">
+      <h1>CHALUKYAS RULED TEMPLE(750 to 973CE)
+        </h1>
+       <p>Sri Rajajeshwara Temple is one of the most famous Hindu temple in Telangana
+         dedicated to lord shiva.Historicallythe region was the capital of the Vemulawada chalukyas.
+         shiva ratri,sri rama navami and Dasara are most famous festivals celebrated every year 
+          </p> 
+          </div>
+
+  );
+}
+export default Note;
